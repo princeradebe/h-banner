0ad5698d19c7dae924ab77b8aeb3216efc84553a
Prince Radebe <princeradebe@gmail.com>
2023-02-20 23:49:28 +0200

    Adds buttons, texts and background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-600 underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col w-full h-[776px] justify-center pl-[211px] bg-hero-pattern">
+      <h6 className="text-hubRed text-lg">Monolith</h6>
+      <h1 className="text-white text-[56px] leading-[64px]">With Monolith's AI<br /> Software, test less and <br />learn more</h1>
+      <p className="text-lg text-white">Spend less time running expensive, repetitive tests and more time learning from your engineering data to predict the exact tests to run.</p>
+      <div>
+        <button className="px-[40px] py-[16px] bg-hubRed hover:bg-hubRedDark rounded-full mr-4 transition-all ease-in duration-300">REQUEST DEMO</button>
+        <button className="outline-button px-[40px] py-[16px] border-[3px] border-hubRed hover:bg-hubRed hover:text-black rounded-full transition-all ease-in duration-300">SPEAK TO OUR TEAM</button>
+      </div>
+      {/* Logo slider */}
+      <div></div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      hubRed: '#EC6664',
+      hubRedDark: '#cc4d59',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/src/Assets/Images/Group_39403_min.png')",
+      },
+    },
   },
   plugins: [],
 }
+
