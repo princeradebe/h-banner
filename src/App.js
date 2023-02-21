55c32974d41a63a1e81075a9761235af0318e2b6
Prince Radebe <princeradebe@gmail.com>
2023-02-22 00:33:26 +0200

    Add mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
+import React from 'react';
 import logoList from './LogoList';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 5000,
+    cssEase: "ease-out",
+    arrows: false,
+    variableWidth: true,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          arrows: true,
+          dots: true,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="flex flex-col w-full h-[776px] justify-center pl-[211px] bg-hero-pattern font-display">
-      <div className="w-[640px]">
+    <div className="flex flex-col pt-[136px] w-full h-[776px] justify-center pl-8 xl:pl-[390px] bg-hero-pattern bg-[#000] bg-opacity-70 bg-blend-hard-light font-display">
+      {/* Top Section */}
+      <div className="w-[420px] xl:w-[640px]">
         <h6 className="text-hubRed text-lg  font-bold">Monolith</h6>
-        <h1 className="text-white text-[56px] leading-[64px] font-normal">With Monolith's AI Software, test less and learn more</h1>
+        <h1 className="text-white text-[56px] leading-[64px] font-normal">With Monolith's AI Software, test less and learn more.</h1>
         <p className="text-lg text-white font-normal mt-4">Spend less time running expensive, repetitive tests and more time learning from your engineering data to predict the exact tests to run.</p>
       </div>
       <div className="mt-8">
@@ -13,16 +60,14 @@ function App() {
         <button className="outline-button px-[40px] py-[16px] border-[3px] border-hubRed hover:bg-hubRed hover:text-black rounded-full transition-all ease-in duration-300">SPEAK TO OUR TEAM</button>
       </div>
       {/* Logo slider */}
-      <div className="flex gap-12 pl-8 w-[1000px] mt-8 overflow-x-hidden">
-        {
-          logoList.map(logo => {
-            return (
-              <img key={logo.id} src={logo.image} alt={logo.name} />
-            );
-          })
-        }
-      </div>
-    </div>
+      <Slider className='flex items-center justify-center w-[360px] xl:w-[1140px] ml-12 mt-12 xl:mt-24 mb-12' {...settings}>
+        {logoList.map(logo => (
+          <div key={logo.id} >
+            <img className='h-4 xl:h-8' src={logo.image} alt={logo.name} />
+          </div>
+        ))}
+      </Slider >
+    </div >
   );
 }
 
--- src/LogoList.js
+++ src/LogoList.js
@@ -1,8 +1,8 @@
 const logoList = [
     {
         id: 1,
-        name: 'Aptar Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        name: 'BMW Logo',
+        image: 'https://www.monolithai.com/hs-fs/hubfs/BMW_logo_(gray)_4_w_66.png?width=198&height=198&name=BMW_logo_(gray)_4_w_66.png',
     },
     {
         id: 2,
@@ -11,49 +11,45 @@ const logoList = [
     },
     {
         id: 3,
-        name: 'BMW Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        name: 'Mercedes Benz Logo',
+        image: 'https://www.monolithai.com/hs-fs/hubfs/mercedes-benz_logo.png?width=344&height=86&name=mercedes-benz_logo.png',
     },
     {
         id: 4,
         name: 'Honda Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        image: 'https://www.monolithai.com/hubfs/honda_logo.svg',
     },
     {
         id: 5,
-        name: 'Honeywell Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        name: 'Siemens Logo',
+        image: 'https://www.monolithai.com/hubfs/Siemens_logo.svg',
     },
     {
         id: 6,
-        name: 'JOTA Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        name: 'Honeywell Logo',
+        image: 'https://www.monolithai.com/hs-fs/hubfs/honeywell-1-logo-black-and-white.png?width=193&height=36&name=honeywell-1-logo-black-and-white.png',
     },
     {
         id: 7,
         name: 'Kautex Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        image: 'https://www.monolithai.com/hs-fs/hubfs/Kautex_Textron_Logo_Bw-1.png?width=193&height=58&name=Kautex_Textron_Logo_Bw-1.png',
     },
     {
         id: 8,
-        name: 'Mercedes Benz Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        name: 'Michelin Logo',
+        image: 'https://www.monolithai.com/hs-fs/hubfs/michelin-tire-logo.png?width=193&height=57&name=michelin-tire-logo.png',
     },
     {
         id: 9,
-        name: 'Michelin Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        name: 'Aptar Logo',
+        image: 'https://www.monolithai.com/hubfs/Aptar%20Logo.svg',
     },
     {
         id: 10,
-        name: 'Siemens Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
+        name: 'Jota Logo',
+        image: 'https://www.monolithai.com/hs-fs/hubfs/JOTA_logo.png?width=840&height=144&name=JOTA_logo.png',
     },
-    {
-        id: 11,
-        name: 'Thales Logo',
-        image: 'https://www.monolithai.com/hubfs/bae-systems_logo.svg',
-    }
+
 ]
 
 export default logoList
